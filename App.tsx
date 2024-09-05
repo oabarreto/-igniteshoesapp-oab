cd3667fe025df652aa66611383312bcfb8fa96bd
Otávio Barreto <oabarreto@ichurch.com.br>
2024-09-05 19:12:27 -0300

    too many things to remenber lol

--- App.tsx
+++ App.tsx
@@ -1,13 +1,21 @@
-import { StatusBar } from 'react-native';
-import { NativeBaseProvider } from 'native-base';
-import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { StatusBar } from "react-native";
+import { NativeBaseProvider } from "native-base";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+import { OneSignal } from "react-native-onesignal";
 
-import { Routes } from './src/routes';
+import { Routes } from "./src/routes";
 
-import { THEME } from './src/theme';
-import { Loading } from './src/components/Loading';
+import { THEME } from "./src/theme";
+import { Loading } from "./src/components/Loading";
 
-import { CartContextProvider } from './src/contexts/CartContext';
+import { CartContextProvider } from "./src/contexts/CartContext";
+
+OneSignal.initialize("bba17935-088c-47ac-9a16-57ccc5ef05bd");
+OneSignal.Notifications.requestPermission(true);
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
